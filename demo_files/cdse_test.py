1ba0bf227ab1c7f1eccd86e731b58e5d5c15fb35
Paul Höhn <paul.hoehn@dlr.de>
2024-07-06 20:21:28 +0200

    Adding some demo files

--- demo_files/cdse_test.py
+++ demo_files/cdse_test.py
@@ -11,13 +11,12 @@ arguments = dict(
     resolution=20,
     collection='SENTINEL-2', 
     start_date='2021-06-01', 
-    end_date='2021-08-10', 
-    download_folder=r'C:\Users\hoeh_pa\Desktop\EOVoxelCraft\demo_files\data\downloads\S2', 
+    end_date='2021-07-15', 
+    download_folder=r'C:\Users\hoeh_pa\Desktop\EOVoxelCraft\demo_files\data\downloads', 
     num_workers=8
     )
 
 items = crafter.search(**arguments)
-item_sel = [item for item in items if item.properties['processingLevel'] == "S2MSIL2A" and item.properties['cloudCover'] < 10.0]
-import pdb; pdb.set_trace()
-cube = crafter.download(item_sel, create_minicubes=True, delete_zip=True)
+item_sel = [item for item in items if item.properties["tileId"] == "32UQU" and item.properties['processingLevel'] == "S2MSI2A" and item.properties['cloudCover'] < 10.0]
+cube = crafter.download(item_sel, create_minicube=True, delete_zip=True)
 import pdb; pdb.set_trace()
