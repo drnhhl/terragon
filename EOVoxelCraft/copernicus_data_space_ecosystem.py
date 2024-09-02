5af5536ea45ab8d178888f3cf8cd6fea3055c436
Paul Höhn <paul.hoehn@dlr.de>
2024-09-02 09:58:54 +0200

    Updated access token generation

--- EOVoxelCraft/copernicus_data_space_ecosystem.py
+++ EOVoxelCraft/copernicus_data_space_ecosystem.py
@@ -1,26 +1,28 @@
 import json
 import requests
 from pystac_client import Client
+from pystac import ItemCollection
 from urllib.parse import urljoin
 from datetime import datetime, timedelta
 from pathlib import Path
 from joblib import Parallel, delayed
 import xarray as xr
+import odc.stac
 
 from .utils import stack_cdse_bands, preprocess_download_task, unzip_files
 from .crafter import VoxelCrafter
 
 class CDSE(VoxelCrafter):
-    def __init__(self, credentials_path:str=None, base_url:str="https://catalogue.dataspace.copernicus.eu/stac/"):
+    def __init__(self, credentials:dict=None, base_url:str="https://catalogue.dataspace.copernicus.eu/stac/"):
         super().__init__()
         self.base_url = base_url
-        self.credentials = {}
-        if credentials_path:
-            self.set_credentials(credentials_path)
+        self.credentials = credentials
+        if credentials:
+            self.access_token = self.get_access_token()
 
-    def set_credentials(self, credentials_path:str):
-        with open(credentials_path, "r") as file:
-            self.credentials = json.load(file) # will store username and password
+    def set_access_token(self, credentials):
+        self.credentials = credentials
+        self.access_token = self.get_access_token()
 
     def retrieve_collections(self, filter_by_name: str=None):
         collections_url = urljoin(self.base_url, "collections")
@@ -30,17 +32,20 @@ class CDSE(VoxelCrafter):
             data = response.json()
             collections = [collection['id'] for collection in data['collections']]
             if filter_by_name:
-                collections = [collection for collection in collections if filter_by_name in collection]
+                collections = [collection for collection in collections if filter_by_name in collection.lower()]
             return collections
         else:
             raise RuntimeError("Failed to retrieve collections")
 
     def search(self, **kwargs):
         super().search(**kwargs)
+        if self.access_token:
+            headers = {"Authorization": f"Bearer {self.access_token}"}
+            catalog = Client.open(self.base_url, headers=headers)
+        else: 
+            catalog = Client.open(self.base_url)
+        
         bounds_4326 = list(self.get_param('shp', raise_error=True).bounds.values[0])
-
-        catalog = Client.open(self.base_url)
-
         start_date = datetime.strptime(self.get_param('start_date'), "%Y-%m-%d") if self.get_param('start_date') else datetime.now()
         end_date = datetime.strptime(self.get_param('end_date'), "%Y-%m-%d") if self.get_param('end_date') else datetime.now() + timedelta(days=10)
         time_interval =f"{start_date.isoformat()}Z/{end_date.isoformat()}Z" if start_date and end_date else None
@@ -52,11 +57,13 @@ class CDSE(VoxelCrafter):
         )
 
         items = search.item_collection()
+
         if len(items) == 0:
             raise ValueError(f"No items found")
+
         return items
 
-    def get_cdse_access_token(self):
+    def get_access_token(self):
         data = {
             "client_id": "cdse-public",
             "username": self.credentials['username'],
@@ -70,25 +77,20 @@ class CDSE(VoxelCrafter):
             raise Exception(f"Access token creation failed. Reponse from the server was: {r.json()}")
         return r.json()["access_token"]
 
-    def build_minicube(self, output_dir):
-        if isinstance(output_dir, str):
-            output_dir = Path(output_dir)
+    def build_minicube(self, output_dir:Path):
+        
+        output_dir = Path(output_dir) if isinstance(output_dir, str) else output_dir
 
-        if len([file for file in Path(output_dir).iterdir()]) < 1:
+        if not any(output_dir.iterdir()):
             raise ValueError("No folders with data provided.")
 
         gdf = self.get_param('shp')
-        num_workers = self.get_param('num_workers', 1)
         res = self.get_param('resolution', 10)
 
         img_folders = list(output_dir.glob(f"**/IMG_DATA/R{res}m"))
         
-        # Create a list of delayed tasks
-        for img_folder in img_folders:
-            ds = stack_cdse_bands(img_folder, gdf, res)
-        
         tasks = [delayed(stack_cdse_bands)(img_folder, gdf, res) for img_folder in img_folders]
-        datasets = Parallel(n_jobs=num_workers)(tasks)
+        datasets = Parallel(n_jobs=self.get_param('num_workers', 1))(tasks)
 
         # Concatenate datasets along the 'time' dimension and process the combined dataset
         ds = xr.concat(datasets, dim='time').compute()
@@ -99,24 +101,59 @@ class CDSE(VoxelCrafter):
 
         return ds
     
-    def download_cdse_file(self, url, file_path):
-        access_token = self.get_cdse_access_token()
-        headers = {"Authorization": f"Bearer {access_token}"}
+    def download_file(self, url, file_path, block_size=32768, max_retries=3):
+        assert self.access_token, "No access token for download; please call set_access_token()."
 
-        with requests.Session() as session:
-            session.headers.update(headers)
-            response = session.get(url, stream=True)
-            block_size = 8192  # 8 Kilobytes
+        headers = {"Authorization": f"Bearer {self.access_token}"}
+        retry_count = 0
+        
+        while retry_count < max_retries:
+            with requests.Session() as session:
+                session.headers.update(headers)
+                response = session.get(url, stream=True)
 
-            if response.status_code == 200:
-                with open(file_path, "wb") as file:
-                    for chunk in response.iter_content(chunk_size=block_size):
-                        if chunk:
-                            file.write(chunk)
-            else:
-                print(f"Failed to download file. Status code: {response.status_code}")
-                print(response.text)
-        return file_path
+                # Handle token expiration by checking for a 401 response
+                if response.status_code == 401 and "Expired signature" in response.text:
+                    print("Access token expired. Refreshing token and retrying...")
+                    self.access_token = self.get_access_token()  # Refresh token
+                    headers["Authorization"] = f"Bearer {self.access_token}"
+                    retry_count += 1
+                    continue  # Retry the request with new token
+
+                # Handle successful response
+                if response.status_code == 200:
+                    with open(file_path, "wb") as file:
+                        for chunk in response.iter_content(chunk_size=block_size):
+                            if chunk:
+                                file.write(chunk)
+                    return file_path  # Return path if successful
+
+                # Handle other HTTP errors
+                else:
+                    print(f"Failed to download file. Status code: {response.status_code}")
+                    print(response.text)
+                    return None
+        
+        # If maximum retries are exceeded
+        print(f"Failed to download after {max_retries} attempts due to repeated authorization errors.")
+        return None
+    
+    def download_data_from_stac(self, items):
+
+        bounds = list(self.get_param('shp', raise_error=True).bounds.values[0])
+        crs = self.get_param('shp', raise_error=True).crs
+        resolution = self.get_param('resolution', raise_error=True)
+
+        # Load data using odc-stac with the authenticated session
+        data = odc.stac.load(
+            items,
+            crs=crs,
+            resolution=resolution,
+            x=(bounds[0], bounds[2]),
+            y=(bounds[1], bounds[3])
+        )
+        
+        return data
 
     def download(self, items, create_minicube=True, delete_zip=True):
 
@@ -124,17 +161,19 @@ class CDSE(VoxelCrafter):
         output_dir.mkdir(parents=True, exist_ok=True)
                 
         tasks = preprocess_download_task(items, output_dir) 
+        max_imgs_parallel = 4
+        num_workers = self.get_param('num_workers', 1)
 
         zip_files = []
            
-        for i in range(0, len(tasks), 4):
+        for i in range(0, len(tasks), max_imgs_parallel):
             batch = tasks[i:i+4] 
-            futures = Parallel(n_jobs=self.get_param('num_workers', 1))(delayed(self.download_cdse_file)(*task) for task in batch)
-            for future in futures:
+            results = Parallel(n_jobs=num_workers)(delayed(self.download_file)(*task) for task in batch)
+            for result in results:
                 try:
-                    result = future.result()
                     if result:
                         zip_files.append(result)
+                        print(f"Downloaded item: {result}")
                 except Exception as e:
                     print(f"Failed to download file with error: {e}")
         
