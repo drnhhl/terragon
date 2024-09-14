2ceb7cca93854e6a957a5aa8d3e006ca0cf320b0
Adrian Höhl <adrian.hoehl@tum.de>
2024-09-14 17:00:48 +0000

    rm asf, cdse, terrabyte

--- terragon/init.py
+++ terragon/init.py
@@ -6,15 +6,6 @@ def init(api, credentials=None, **kwargs):
     elif api == 'gee' or api == 'earthengine':
         from .google_earth_engine import GEE
         return GEE(credentials, **kwargs)
-    elif api == 'cdse' or api == 'copernicus':
-        from .copernicus_data_space_ecosystem import CDSE
-        return CDSE(credentials, **kwargs)
-    elif api == 'asf' or api == 'alaska_satellite_facility':
-        from .alaska_satellite_facility import ASF
-        return ASF(credentials, **kwargs)
-    elif api == 'tb' or api == 'terrabyte':
-        from .dlr_terrabyte import TB
-        return TB(credentials, **kwargs)
     else:
-        raise ValueError(f'API {api} not supported. Please use "pc", "gee", "cdse", "asf" or "tb".')
+        raise ValueError(f'API {api} not supported. Please use "pc" or "gee".')
 
