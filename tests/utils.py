e57396367f2d14346cebd9389019c7028d098367
Adrian Höhl <adrian.hoehl@tum.de>
2024-09-14 17:20:16 +0000

    enable local loading of env vars

---
+++ tests/utils.py
@@ -0,0 +1,8 @@
+import os
+
+def load_env_variables():
+    # Check if running in GitHub Actions
+    if os.getenv('GITHUB_ACTIONS') != 'true':
+        # Load environment variables
+        from dotenv import load_dotenv
+        load_dotenv()
