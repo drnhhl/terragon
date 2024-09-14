22e940935164aa39edd714d16d4e14f251e9f697
Adrian Höhl <adrian.hoehl@tum.de>
2024-09-14 10:43:01 +0000

    add code to create gee credentials in workflow

---
+++ .github/gee_token.py
@@ -0,0 +1,11 @@
+# workflow taken from https://github.com/gee-community/geemap/blob/master/.github/ee_token.py
+# https://github.com/gee-community/geemap/discussions/1341
+import os
+
+ee_token = os.environ["EARTHENGINE_TOKEN"]
+credential = '{"refresh_token":"%s"}' % ee_token
+credential_file_path = os.path.expanduser("~/.config/earthengine/")
+os.makedirs(credential_file_path, exist_ok=True)
+with open(credential_file_path + "credentials", "w") as file:
+    file.write(credential)
+
