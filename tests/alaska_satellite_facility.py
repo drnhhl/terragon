cac8dc8530f68b23e2a422ac6731a91f560b6827
Paul Höhn <paul-hoehn@outlook.de>
2025-04-11 18:18:17 +0200

    Faster tests

--- tests/alaska_satellite_facility.py
+++ tests/alaska_satellite_facility.py
@@ -24,19 +24,25 @@ class TestASF(_TestBase, unittest.TestCase):
         self.arguments["num_workers"] = 4
         self.arguments["rm_tmp_files"] = False
 
+    def test_crs(self):
+        pass
+
+    def test_resolution(self):
+        pass
+
     def test_alos_palsar(self):
         args = self.arguments.copy()
         args["collection"] = "ALOS PALSAR"
-        args["start_date"] = "2009-01-01"
-        args["end_date"] = "2009-12-31"
-        args["bands"] = ["HH", "HV"]
+        args["start_date"] = "2009-10-22"
+        args["end_date"] = "2009-10-23"
+        args["bands"] = ["HH"]
         args["resolution"] = 10
         args["filter"] = {"processingLevel": "L2.2"}
 
         ds = self.tg.create(**args)
 
         self.assertTrue(
-            len(ds.time) == 3  # 3 items in 2009
+            len(ds.time) == 1 
             and self.width - 1 <= len(ds.x) <= self.width + 1
             and self.height - 1 <= len(ds.y) <= self.height + 1
         )
@@ -44,16 +50,16 @@ class TestASF(_TestBase, unittest.TestCase):
     def test_alos_avnir2(self):
         args = self.arguments.copy()
         args["collection"] = "ALOS AVNIR-2"
-        args["start_date"] = "2009-01-01"
-        args["end_date"] = "2009-12-31"
-        args["bands"] = ["IMG-01", "IMG-02", "IMG-03", "IMG-04"]
+        args["start_date"] = "2009-12-19"
+        args["end_date"] = "2009-12-21"
+        args["bands"] = ["IMG-01"] # IMG_02, IMG_03, IMG_04
         args["resolution"] = 10
         args["filter"] = {}
 
         ds = self.tg.create(**args)
 
         self.assertTrue(
-            len(ds.time) == self.nr_time_steps
+            len(ds.time) == 1
             and self.width - 1 <= len(ds.x) <= self.width + 1
             and self.height - 1 <= len(ds.y) <= self.height + 1
         )
