60d5f2a0903a12034674cb99ccb858c3bcc0f2c2
Paul Höhn <paul-hoehn@outlook.de>
2025-04-22 10:16:49 +0200

    Update for single band visualization

--- docs/demo_files/utils.py
+++ docs/demo_files/utils.py
@@ -5,15 +5,22 @@ import numpy as np
 def visualize_sat_images(da, gdf, bands):
     assert 0 < len(bands) < 4  # up to 3 bands
 
-    fig, axs = plt.subplots(1, len(da.time))
+    fig, axs = plt.subplots(1, len(da.time), figsize=(5 * len(da.time), 5))
 
     if not isinstance(axs, np.ndarray):
         axs = np.array([axs])
 
     for i, ax in enumerate(axs):
-        da.isel(time=i)[bands].to_array().plot.imshow(robust=True, ax=ax)
+        subset = da.isel(time=i)
+        if len(bands) == 1:
+            subset[bands[0]].plot.imshow(robust=True, ax=ax, cmap='gray')
+        else:
+            subset[bands].to_array().plot.imshow(robust=True, ax=ax)
+
         gdf.plot(ax=ax, facecolor="none", edgecolor="red")
-        ax.set_title(da.time[i].dt.strftime("%Y-%m-%d").values)
+        ax.set_title(str(subset.time.dt.strftime("%Y-%m-%d").values))
         ax.set_axis_off()
         ax.autoscale()
-    fig.show()
+
+    plt.tight_layout()
+    plt.show()
